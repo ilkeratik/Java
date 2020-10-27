17f6ad21b41bac3a8fa10b2952d7e2027ed5eef2
İlker Atik <44725644+ilkeratik@users.noreply.github.com>
2020-10-27 10:48:38 +0300

    refactored and tidy

--- hottestday.java
+++ hottestday.java
@@ -6,25 +6,29 @@ import java.io.File;
 public class hottestday{
 
     public static CSVRecord theday(FileResource fr){ //finds the biggest value in a particular file
-       
-        double currHigh, currVal; //will be used for compare values among records
-        CSVParser csv = fr.getCSVParser(); //creating csvparser from file resource
-        CSVRecord highest = null;
+
+        CSVParser csv = fr.getCSVParser();
+        CSVRecord currHigh = null, currVal = null;
         
         for (CSVRecord rec : csv){
-            if(highest == null){
-                highest = rec;
-            }
-            else {
-                currVal = Double.parseDouble(rec.get("TemperatureF"));
-                currHigh = Double.parseDouble(highest.get("TemperatureF"));
-                if (currVal > currHigh){ //checks whether currVal is bigger than last highest point.
-                    highest = rec;
-                }
-
-            }
+            currHigh = compare_two(currHigh, rec);
         }
-        return highest;
+        return currHigh;
+    }
+    public static CSVRecord compare_two(CSVRecord currHigh, CSVRecord currVal){//refactored version of the common operation
+        
+        if(currHigh == null){
+            currHigh = currVal;
+        }
+        else {
+            double curv = Double.parseDouble(currVal.get("TemperatureF"));
+            double curh = Double.parseDouble(currHigh.get("TemperatureF"));
+
+            if(curv > curh){//checks whether currVal is bigger than last highest point.
+                currHigh = currVal;
+            }                                 
+        }
+        return currHigh;
     }
 
     public static void largest_multiple(){ //checks multiple(many) files to find biggest value
@@ -37,27 +41,14 @@ public class hottestday{
             FileResource fr = new FileResource(f);
 
             currVal = theday(fr);
-            if(currHigh == null){//same process of comparing values
-                currHigh = currVal;
-            }
-            else {
-                double curv = Double.parseDouble(currVal.get("TemperatureF"));
-                double curh = Double.parseDouble(currHigh.get("TemperatureF"));
-
-                if(curv > curh){
-                    currHigh = currVal;
-                }                                 
-            }
+            currHigh = compare_two(currHigh, currVal);
+           
         }
-
-        System.out.println("date:" +currHigh.get("DateUTC") +"time:"+ currHigh.get("TimeEDT")  +" temp was:"  +currHigh.get("TemperatureF"));
-
+        System.out.println("date:" +currHigh.get("DateUTC") +" time:"+ currHigh.get("TimeEST")  +" temp was:"  +currHigh.get("TemperatureF"));
     }
 
     public static void main (String args[]){
         largest_multiple();            
-
     }
-
-
 }
+
